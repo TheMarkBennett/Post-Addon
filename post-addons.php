740bf3e92f52acc2b4ff62e0e634b209be0293ff
Mark Bennett <mark.bennett@ucf.edu>
2018-11-21 17:52:32 -0500

    Plugin Header File
    Adds Plugin header File

---
+++ post-addons.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: UCF Post List Addon
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: Adds a shortcode/templates for more post listing and people styles and templates
+ * Version: 1.0 beta
+ * Author: Mark Bennett
+ * Author URI: http://www.mywebsite.com
+ */
+
+ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
