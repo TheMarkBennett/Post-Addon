b87c76411a783d4762111ac6b1eedb20fcf1d984
Mark Bennett <mark.bennett@ucf.edu>
2018-11-21 19:40:45 -0500

    updated version

--- post-addons.php
+++ post-addons.php
@@ -3,7 +3,7 @@
  * Plugin Name: UCF Post List Addon
  * Plugin URI: http://www.mywebsite.com/my-first-plugin
  * Description: Adds a shortcode/templates for more post listing and people styles and templates
- * Version: 1.0 beta
+ * Version: 1.01 beta
  * Author: Mark Bennett
  * Author URI: http://www.mywebsite.com
  */
