c4a2c0a47055dd7e933f39baaec5268edd861177
Mark Bennett <mark.bennett@ucf.edu>
2018-11-21 20:21:34 -0500

    remove extra data

--- includes/people-template.php
+++ includes/people-template.php
@@ -2,9 +2,6 @@
 //filter for people template
 add_filter( 'single_template', 'load_my_custom_template', 50, 1 );
 function load_my_custom_template( $template ) {
-
-	var_dump("Working...");
-
 		if ( is_singular( 'person' ) ) {
 		$template = plugins_url( 'templates/person.php', __FILE__ );
 	}
