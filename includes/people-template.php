5e6e70232ebc90602040c1857e27bf7e6f54d2c7
Mark Bennett <mark.bennett@ucf.edu>
2018-11-21 20:02:01 -0500

    testing template

--- includes/people-template.php
+++ includes/people-template.php
@@ -1,13 +1,10 @@
 <?php
 
-$jj = get_post_type( $post_id );
-
-	var_dump($jj);
-	echo $jj;
-
 //filter for people template
 add_filter( 'single_template', 'load_my_custom_template', 50, 1 );
 function load_my_custom_template( $template ) {
+
+	var_dump("Working...");
 
 		if ( is_singular( 'person' ) ) {
 		$template = plugins_url( 'templates/person.php', __FILE__ );
